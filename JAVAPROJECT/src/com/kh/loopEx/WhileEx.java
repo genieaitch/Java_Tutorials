40c689c932ef2d9433d0a5ffaf1e76099b66ce9f
genieaitch <hah4815@naver.com>
2024-12-03 11:41:10 +0900

    배열선언, 객체생성, 배열객체, array, nation

--- JAVAPROJECT/src/com/kh/loopEx/WhileEx.java
+++ JAVAPROJECT/src/com/kh/loopEx/WhileEx.java
@@ -38,4 +38,14 @@ public class WhileEx {
         System.out.println("합계 : " + sum);
     }
 
+    /*
+    do-while()
+    - 조건식이 뒤에 작성된 while문
+    - true가 아니더라도 최초 1회 이상의 반복을 보장
+
+    do {
+        최초 1회는 무조건 수행하되, 2회부터는 while문이 true이어야 실행
+    } while(조건);
+    */
+
 }
---
+++ JAVAPROJECT/src/com/kh/methodEx/MethodEx.java
@@ -0,0 +1,12 @@
+package com.kh.methodEx;
+
+public class MethodEx {
+
+    //특정 행동이나 기능을 작성하는 공간
+
+    /*
+    접근제한자 static 전달유무 기능명칭(매개변수명) {
+        특정 기능이나 반환 값 작성
+    }
+    */
+}
